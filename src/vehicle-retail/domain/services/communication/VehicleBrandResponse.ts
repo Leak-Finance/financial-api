5f236338a3e735ab044ddf0e6227249e986d2116
Andrés Leiva <aleiva1700@gmail.com>
2023-11-04 11:46:08 -0500

    feat: add vehicle brand resources and response

---
+++ src/vehicle-retail/domain/services/communication/VehicleBrandResponse.ts
@@ -0,0 +1,4 @@
+import {BaseResponse} from "@app/shared/domain/service/communication/BaseResponse";
+import {VehicleBrand} from "@app/vehicle-retail/domain/model/VehicleBrand.entity";
+
+export class VehicleBrandResponse extends BaseResponse<VehicleBrand> {}
