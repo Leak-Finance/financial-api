3d12e8950cd580cb8213ed0983203170b982f046
Andrés Leiva <aleiva1700@gmail.com>
2023-11-04 10:35:17 -0500

    feat: add vehicle entity

---
+++ src/vehicle-retail/domain/model/Vehicle.entity.ts
@@ -0,0 +1,20 @@
+import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {VehicleBrand} from "@app/vehicle-retail/domain/model/VehicleBrand.entity";
+
+@Entity('vehicles')
+export class Vehicle {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  model: string;
+
+  @Column()
+  manufactureYear: number;
+
+  @Column()
+  photoUrl: string;
+
+  @ManyToOne(() => VehicleBrand)
+  brand: VehicleBrand;
+}
