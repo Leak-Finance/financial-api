33fba01484a38c9be5e5d7e9f0e1685df55b66d5
Oliver Tuesta <oliver.jtuesta@gmail.com>
2023-11-07 14:21:43 -0500

    feat: add post put and getbyid endpoints to currency.controller.ts

--- src/vehicle-retail/interfaces/rest/Currency.controller.ts
+++ src/vehicle-retail/interfaces/rest/Currency.controller.ts
@@ -1,84 +1,5 @@
-/*
-@ApiBearerAuth()
-@ApiTags('Vehicle Brands')
-@Controller('api/v1/vehicle-retail/vehicle-brands')
-export class VehicleBrandsController {
-  constructor(
-    @Inject(VehicleBrandService) private readonly vehicleBrandService: VehicleBrandService,
-    private readonly vehicleBrandMapper: VehicleBrandMapper
-  ) { }
-
-  @Get()
-  @ApiOperation({ summary: 'Get all vehicle brands.' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Vehicle brand list retrieved successfully.',
-    type: VehicleBrandResource,
-    isArray: true,
-  })
-  @ApiUnauthorizedResponse({
-    description: 'Unauthorized request.',
-  })
-  async getAll() {
-    return this.vehicleBrandMapper.toResourceArray(await this.vehicleBrandService.getAll());
-  }
-
-  @Get(':vehicleBrandId')
-  @ApiOperation({ summary: 'Get a vehicle brand by id.' })
-  async getById(@Param('vehicleBrandId', ParseIntPipe) vehicleBrandId: number) {
-    const result = await this.vehicleBrandService.getById(vehicleBrandId);
-
-    if (!result.success) {
-      throw new NotFoundException([result.message]);
-    }
-
-    return this.vehicleBrandMapper.toResource(result.resource);
-  }
-
-  @Post()
-  @ApiOperation({ summary: 'Create a new vehicle brand.' })
-  async create(@Body() createVehicleBrandResource: CreateVehicleBrandResource) {
-    const model = this.vehicleBrandMapper.toModel(createVehicleBrandResource);
-    const result = await this.vehicleBrandService.create(model);
-
-    if (!result.success) {
-      throw new BadRequestException([result.message]);
-    }
-
-    return this.vehicleBrandMapper.toResource(result.resource);
-  }
-
-  @Put(':vehicleBrandId')
-  @ApiOperation({ summary: 'Update a vehicle brand.' })
-  async update(
-    @Param('vehicleBrandId', ParseIntPipe) vehicleBrandId: number,
-    @Body() updateVehicleBrandResource: UpdateVehicleBrandResource
-  ) {
-    const result = await this.vehicleBrandService.update(vehicleBrandId, updateVehicleBrandResource);
-
-    if (!result.success) {
-      throw new NotFoundException([result.message]);
-    }
-
-    return this.vehicleBrandMapper.toResource(result.resource);
-  }
-
-  @Delete(':vehicleBrandId')
-  @ApiOperation({ summary: 'Delete a vehicle brand.' })
-  async delete(@Param('vehicleBrandId', ParseIntPipe) vehicleBrandId: number) {
-    const result = await this.vehicleBrandService.delete(vehicleBrandId);
-
-    if (!result.success) {
-      throw new NotFoundException([result.message]);
-    }
-
-    return this.vehicleBrandMapper.toResource(result.resource);
-  }
-}
-*/
-
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
-import { Controller, Get, HttpStatus, Inject } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, HttpStatus, Inject, NotFoundException, Param, Post, Put } from "@nestjs/common";
 import { CurrencyService } from "@app/vehicle-retail/domain/services/CurrencyService";
 import { CurrencyMapper } from "@app/vehicle-retail/interfaces/rest/mapper/CurrencyMapper";
 import { CurrencyResource } from "@app/vehicle-retail/interfaces/rest/resource/currency/CurrencyResource";
@@ -105,5 +26,60 @@ export class CurrencyController {
   })
   async getAll() {
     return this.currencyMapper.toResourceArray(await this.currencyService.getAll());
+  }
+
+  @Get(":id")
+  @ApiOperation({ summary: "Get currency by id." })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "Currency retrieved successfully.",
+    type: CurrencyResource,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unauthorized request.",
+  })
+  async getById(@Param("id") id: number) {
+    const result = await this.currencyService.getById(id);
+    if (!result.success) {
+      throw new NotFoundException([result.message]);
+    }
+    return this.currencyMapper.toResource(result.resource);
+  }
+
+  @Post()
+  @ApiOperation({ summary: "Create currency." })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: "Currency created successfully.",
+    type: CurrencyResource,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unauthorized request.",
+  })
+  async create(@Body() currency: CurrencyResource) {
+    const model = this.currencyMapper.toModel(currency);
+    const result = await this.currencyService.create(model);
+    if (!result.success) {
+      throw new BadRequestException([result.message]);
+    }
+    return this.currencyMapper.toResource(result.resource);
+  }
+
+  @Put(":id")
+  @ApiOperation({ summary: "Update currency." })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "Currency updated successfully.",
+    type: CurrencyResource,
+  })
+  @ApiUnauthorizedResponse({
+    description: "Unauthorized request.",
+  })
+  async update(@Param("id") id: number, @Body() currency: CurrencyResource) {
+    const result = await this.currencyService.update(id, currency);
+    if (!result.success) {
+      throw new BadRequestException([result.message]);
+    }
+    return this.currencyMapper.toResource(result.resource);
   }
 }
